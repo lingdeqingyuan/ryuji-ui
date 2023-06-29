fab7fb522a74f2b8a63ee9d93fd935b02d5e22d7
ryuji <yrejgnl11@163.com>
2023-06-29 11:51:33 +0800

    feat: finish button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import Button, { ButtonType, ButtonSize } from './components/Button/button';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Button autoFocus>default</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>React hooks</Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Large}>React hooks</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Small} disabled>React hooks</Button>
+      <Button btnType={ButtonType.Link} href='www.baidu.com'>React hooks</Button>
+    </div>
   );
 }
 
